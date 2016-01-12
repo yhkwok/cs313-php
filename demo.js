5d8a15dc1dfcc4ddf1ca3446fb6fa1729ec8dd4e
yhkwok <kwo13001@byui.edu>
2016-01-12 10:05:10 -0700

    more files

---
+++ demo.js
@@ -0,0 +1,21 @@
+function doAnotherAlert() {
+	alert("Another Alert")
+}
+
+function changeColor(){
+	//$("#pageHeaderID").toggle();
+	$("#pageHeaderID").fadeOut();
+	
+	/**
+	var color = document.getElementById("pageHeaderID").style.color;
+	if (color =="blue"){
+		color = "red";
+		//document.getElementById("pageHeaderID").style.color = "red";
+	} else {
+		color = "blue";
+		//document.getElementById("pageHeaderID").style.color = "blue";
+	}
+	
+	document.getElementById("pageHeaderID").style.color = color;
+	**/
+}
