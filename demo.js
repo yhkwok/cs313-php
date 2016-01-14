576a9986332f1e42afee09d978aaf39bdb20141c
yhkwok <kwo13001@byui.edu>
2016-01-13 22:32:16 -0700

    homepage and assignment page, and elements that goes with it

--- demo.js
+++ demo.js
@@ -2,9 +2,18 @@ function doAnotherAlert() {
 	alert("Another Alert")
 }
 
-function changeColor(){
-	//$("#pageHeaderID").toggle();
-	$("#pageHeaderID").fadeOut();
+function toggle(){
+	$("#everything").toggle();
+}
+
+function mouseOver(){
+	$("#link").css("background-color", "yellow");
+}
+function mouseOut(){
+    $("#link").css("background-color", "lightgray");
+}
+    
+	//$("#pageHeaderID").fadeOut();
 	
 	/**
 	var color = document.getElementById("pageHeaderID").style.color;
@@ -18,4 +27,3 @@ function changeColor(){
 	
 	document.getElementById("pageHeaderID").style.color = color;
 	**/
-}
