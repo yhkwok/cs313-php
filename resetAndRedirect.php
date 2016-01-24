7d6a3acda31ad521d81a71dbe0d3904b6bebf590
yhkwok <kwo13001@byui.edu>
2016-01-24 15:00:36 -0700

    more on PHPSurvey

--- resetAndRedirect.php
+++ resetAndRedirect.php
@@ -1,7 +1,13 @@
 <?php
-	session_start();
+	//remove PHPSESSID from browser
+	if ( isset( $_COOKIE[session_name()] ) )
+	setcookie( session_name(), ¡§¡¨, time()-3600, ¡§/¡¨ );
+	//clear session from globals
+	$_SESSION = array();
+	//clear session from disk
 	session_destroy();
-	session_reset();
+	
+	//session_reset();
 	//unset($_SESSION);
 	//$_SESSION["pagecount"] = 1;
 	header('Location: phpSurvey.php');
