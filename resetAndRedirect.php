12b9e02e373677f79c74d146397222ba9f3a2124
yhkwok <kwo13001@byui.edu>
2016-01-24 15:16:31 -0700

    more on PHPSurvey

--- resetAndRedirect.php
+++ resetAndRedirect.php
@@ -1,4 +1,5 @@
 <?php
+	//http://7php.com/php-5-3-how-to-completely-destroy-session-variables-in-php/
 	//remove PHPSESSID from browser
 	if ( isset( $_COOKIE[session_name()] ) )
 	setcookie( session_name(), ¡§¡¨, time()-3600, ¡§/¡¨ );
