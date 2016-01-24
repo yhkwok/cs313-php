714d9dee1bffcf7e89559f7ea2893fac08a44123
yhkwok <kwo13001@byui.edu>
2016-01-24 03:55:13 -0700

    more on PHPSurvey

--- phpSurvey.php
+++ phpSurvey.php
@@ -26,30 +26,35 @@ exit;
 	<title>PHP Survey</title>
 </head>
 
-<body background = 'paosley.png'>
-<button onclick="location.href='surveyResult.php'">
-     Just show me the result</button>
+<body background = 'paisley.png'>
+<h1>This shall be one of the most random survey you have ever done</h1>
+<hr/>
+<div>
+<button onclick="location.href='surveyResult.php'">Just show me the result</button><br/>
+<hr/>
 <form action="handleSurvey.php" method="post">
 <p>Name</p><input type="text" name="name">
-
+<hr/>
 <p>Q1: Have you eaten yet?</p>
 <input type="radio" name="eaten" value="Yes">Yes<br/>
 <input type="radio" name="eaten" value="No">No<br/>
-
+<hr/>
 <p>Q2: Do you like snow?</p>
 <input type="radio" name="snow" value="Yes">Love it(Yes)<br/>
 <input type="radio" name="snow" value="No">O STOP IT!(No)<br/>
-
+<hr/>
 <p>Q3: Is Brother Burton awesome?</p>
 <input type="radio" name="burton" value="Yes">Absolutely!!(Yes)<br/>
 <input type="radio" name="burton" value="No">I don't think so...(No)<br/>
-
+<hr/>
 <p>Q4: Are you handsome?</p>
 <input type="radio" name="handsome" value="Yes">Why am I so handsome?(Yes)<br/>
 <input type="radio" name="handsome" value="No">Nooo...I don't think so(No)<br/>
 <br/>
+<hr/>
 <button type="submit">Submit</button>
 </form>
+</div>
 
 </body>
 
