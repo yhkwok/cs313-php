e4b84b586388d266887f08ce03cffd6a7e965f12
yhkwok <kwo13001@byui.edu>
2016-02-19 00:35:33 -0700

    db modi

--- getCarData.php
+++ getCarData.php
@@ -11,9 +11,9 @@
    </HEAD>
 <BODY>
 <?php
-	if (isset($_POST["makeID"]) and isset($_POST["modelID"]) and isset($_POST["price"]) and isset($_POST["miles"]) and isset($_POST["sellerDisplayName"]) and isset($_POST["sellerEmail"]))
-		echo "<form action=\"insert.php\" method=\"post\">";
-	else
+	//if (isset($_POST["makeID"]) and isset($_POST["modelID"]) and isset($_POST["price"]) and isset($_POST["miles"]) and isset($_POST["sellerDisplayName"]) and isset($_POST["sellerEmail"]))
+		//echo "<form action=\"insert.php\" method=\"post\">";
+	//else
 		echo "<form action=\"\" method=\"post\">";
 		
 		$sql = $db->query('SELECT * FROM makes');
@@ -54,11 +54,29 @@
 			echo "<input type=\"submit\" value=\"Submit\">";
 		else
 			echo "<input type=\"submit\" value=\"Next\">";
-		?>
+		
 	</form> 
-	<?php
-	//if (isset($_POST["makeID"]) and isset($_POST["modelID"]) and isset($_POST["price"]) and isset($_POST["miles"]) and isset($_POST["sellerDisplayName"]) and isset($_POST["sellerEmail"]))
-		//echo $_POST["makeID"] . " " . $_POST["modelID"] . " " . $_POST["price"] . " " . $_POST["miles"] . " " . $_POST["sellerDisplayName"] . " " . $_POST["sellerEmail"];
+		if (isset($_POST["makeID"]) and isset($_POST["modelID"]) and isset($_POST["price"]) and isset($_POST["miles"]) and isset($_POST["sellerDisplayName"]) and isset($_POST["sellerEmail"]))
+		{
+			$price = $_POST["price"];
+			$miles = $_POST["miles"];
+			$sdn = $_POST["sellerDisplayName"];
+			$se = $_POST["sellerEmail"];
+			date_default_timezone_set('America/Denver');
+			$date = date('m/d/Y h:i:s a', time());
+			$modelID = $_POST["modelID"];
+			$insert = $db->prepare('INSERT INTO cars (price, miles, sellerDisplayName, sellerEmail, postDate, modelID) VALUES ($price, $miles, $sdn, $se, $date, $modelID)');
+			$ID = $db->lastInsertId();
+		
+			$query='SELECT * FROM cars where id =' . $ID;
+			$sql = $db->query($query);
+			echo "Below is the entries you have just inserted: ";
+			while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+				echo $row["price"] . " " . 	$row["miles"] . " " . $row["sellerDisplayName"] . " " . $row["sellerEmail"] . " " . $row["postDate"] . " " . $row["modelID"];
+			}
+		
+		}
+
 	?>
 	</body>
 	</html>
