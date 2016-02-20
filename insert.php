34be2db8ee1926d1bdd685c7558a151f4349aab8
yhkwok <kwo13001@byui.edu>
2016-02-20 13:07:36 -0700

    db modi

--- insert.php
+++ insert.php
@@ -8,18 +8,21 @@
 		$miles = $_POST["miles"];
 		$sdn = $_POST["sellerDisplayName"];
 		$se = $_POST["sellerEmail"];
-		date_default_timezone_set('America/Denver');
-		$date = date('m/d/Y h:i:s a', time());
+		$date = date('Y-m-d H:i:s');
 		$modelID = $_POST["modelID"];
-		$insert = $db->prepare('INSERT INTO cars (price, miles, sellerDisplayName, sellerEmail, postDate, modelID) VALUES ($price, $miles, $sdn, $se, $date, $modelID)');
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$query = "INSERT INTO cars 
+		(price, miles, sellerDisplayName, sellerEmail, postDate, modelID) 
+		VALUES ('$price', '$miles', '$sdn', '$se', '$date', '$modelID')";
+		$db->exec($query);
 		$ID = $db->lastInsertId();
 		
 		$query='SELECT * FROM cars where id =' . $ID;
-			$sql = $db->query($query);
-			echo "Below is the entries you have just inserted: ";
-			while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
-				echo $row["price"] . " " . 	$row["miles"] . " " . $row["sellerDisplayName"] . " " . $row["sellerEmail"] . " " . $row["postDate"] . " " . $row["modelID"];
-			}
-		
+		$sql = $db->query($query);
+		echo "Below is the entries you have just inserted: ";
+		while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+			echo $row["price"] . " " . 	$row["miles"] . " " . $row["sellerDisplayName"] . " " . $row["sellerEmail"] . " " . $row["postDate"] . " " . $row["modelID"];
+		}
 	}
+	$_POST["makeID"] = $_POST["modelID"] = $_POST["price"] = $_POST["miles"] = $_POST["sellerDisplayName"] = $_POST["sellerEmail"] = "";
 ?>
